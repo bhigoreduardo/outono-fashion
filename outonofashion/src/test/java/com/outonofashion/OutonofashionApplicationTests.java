70e80d89a3e050778d22931f6b6e3f2f5b96bce8
unknown <bhigoreduardo@gmail.com>
2022-10-27 07:30:35 -0300

    Created Spring Boot backend

---
+++ outonofashion/src/test/java/com/outonofashion/OutonofashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outonofashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutonofashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
